fbe99c49f5b07f145ea80a52e838d7cf3e45efbc
harduserNIKITA <cumpotttt@yandex.ru>
2025-07-14 16:37:51 +0300

    [TASK-3]First version

---
+++ kovshikov.nikita/task-3/script.js
@@ -0,0 +1,21 @@
+function findMedian(arr) {
+    if (arr.length == 0) {
+        return null;
+    }
+
+    const sortedArr = [...arr].sort((a, b) => a - b);
+    const middleIndex = Math.floor(sortedArr.length / 2);
+
+    if (sortedArr.length % 2 != 0) {
+        return sortedArr[middleIndex];
+    }
+    else {
+        return (sortedArr[middleIndex - 1] + sortedArr[middleIndex]) / 2;
+    }
+}
+
+let stringArray = prompt('Задайте массив чисел идущих через пробел');
+let arr = stringArray.split(" ");
+
+console.log('Исходный массив:' + arr);
+console.log('Его медиана:' + findMedian(arr));
