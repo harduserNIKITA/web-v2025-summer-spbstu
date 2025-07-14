abd888ff93b052695330dcf1db0ad33c13ce8f34
harduserNIKITA <cumpotttt@yandex.ru>
2025-07-14 19:37:26 +0300

    [TASK-3]After edits

--- kovshikov.nikita/task-3/script.js
+++ kovshikov.nikita/task-3/script.js
@@ -1,21 +1,19 @@
 function findMedian(arr) {
-    if (arr.length == 0) {
+    if (arr.length === 0) {
         return null;
     }
 
     const sortedArr = [...arr].sort((a, b) => a - b);
     const middleIndex = Math.floor(sortedArr.length / 2);
 
-    if (sortedArr.length % 2 != 0) {
+    if (sortedArr.length % 2 !== 0) {
         return sortedArr[middleIndex];
     }
-    else {
-        return (sortedArr[middleIndex - 1] + sortedArr[middleIndex]) / 2;
-    }
+    return (sortedArr[middleIndex - 1] + sortedArr[middleIndex]) / 2;
 }
 
 let stringArray = prompt('Задайте массив чисел идущих через пробел');
-let arr = stringArray.split(" ");
+let arr = stringArray.split(" ").map(Number);
 
 console.log('Исходный массив:' + arr);
 console.log('Его медиана:' + findMedian(arr));
